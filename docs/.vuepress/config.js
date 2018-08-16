01d656746f0c2a8b403d30a0c25b0b03a84f9f85
wave <372969383@qq.com>
2018-08-17 01:28:54 +0800

    style(调整主题色 code 颜色):

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -42,7 +42,37 @@ module.exports = {
       ['/about-me/', '☀️ 蜜汁彩蛋'],
     ],
     // 显示所有页面的标题链接
-    displayAllHeaders: false
+    displayAllHeaders: false,
+
+    // 更新时间戳
+    lastUpdated: 'Last Updated',
+    serviceWorker: {
+      updatePopup: true // Boolean | Object, 默认值是 undefined.
+      // 如果设置为 true, 默认的文本配置将是: 
+      // updatePopup: { 
+      //    message: "New content is available.", 
+      //    buttonText: "Refresh" 
+      // }
+    },
+
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'ayameng/magic-wpress',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/" Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: 'Open Source',
+
+    // 以下为可选的编辑链接选项
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    // docsRepo: 'vuejs/vuepress',
+
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'master',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: 'Help Me To Do More Good'
   },
 
   configureWebpack: (config, isServer) => {
@@ -52,5 +82,19 @@ module.exports = {
     return {}
   },
 
+  evergreen: true,
+
+  markdown: {
+    // markdown-it-anchor 的选项
+    anchor: { permalink: false },
+    // markdown-it-toc 的选项
+    toc: { includeLevel: [1, 2] },
+    config: md => {
+      // 使用更多的 markdown-it 插件!
+      md.use(require('markdown-it'))
+    }
+  }
+
+  // Only Https
   // serviceWorker: true
 }
