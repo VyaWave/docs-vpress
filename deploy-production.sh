7fc763908f646960c86038505e8e20dc6135174c
wave <372969383@qq.com>
2018-08-19 15:47:54 +0800

    fix(deploy-production.sh): 增加 koa 发布支持

--- deploy-production.sh
+++ deploy-production.sh
@@ -11,6 +11,10 @@ yarn build:production
 # 进入生成的文件夹
 cd docs/.vuepress
 
+# docs.weiya.design
 scp -r dist/* root@123.207.231.183:/var/www/magellan/magic-server/docs
+
+# docs.weiya.live
+scp -r dist/* root@123.207.231.183:/var/www/magellan/magic-server/static
 
 cd -
